088ccf33a72cbaf1bd8a2eae00ca9789528610ad
Christian Emmanuel Yañez Gonzalez <laithg0279@gmail.com>
2022-01-15 03:14:52 -0700

    vistas de editar

--- js/editarAcademia.js
+++ js/editarAcademia.js
@@ -5,9 +5,8 @@ $(document).ready(() => {
         for (let i = 0; i < (4 * 8); i++) {
             datos.push({
                 foto: "img/perfil.png",
-                nomina: "nomina",
-                nombre: "nombre",
-                carrera: "carrera"
+                integrantes: "total_integrantes",
+                nombre: "nombre"
             });
         }
         return datos;
@@ -29,9 +28,9 @@ $(document).ready(() => {
     const selected = estado => estado ? "selected" : "";
 
     const opcionesMostrar = () => {
-        let maximo = buscarCookie("cantidadDocente");
+        let maximo = buscarCookie("cantidadAcademia");
         if (maximo == 0) {
-            document.cookie = `cantidadDocente=4;max-age=60*10;`;
+            document.cookie = `cantidadAcademia=4;max-age=60*10;`;
             maximo = 4;
         }
         $("#mostrar")[0].innerHTML = `
@@ -47,14 +46,14 @@ $(document).ready(() => {
 
     const cargar = () => {
 
-        let mostrar = parseInt(buscarCookie("cantidadDocente"));
+        let mostrar = parseInt(buscarCookie("cantidadAcademia"));
 
         let datos = getDocentes();;
 
         let maximo = Math.ceil(datos.length / mostrar);
-        let pagina = buscarCookie("paginaDocente");
+        let pagina = buscarCookie("paginaAcademia");
         pagina = maximo > pagina ? pagina : 0;
-        document.cookie = `paginaDocente=${pagina};max-age=60*10;`;
+        document.cookie = `paginaAcademia=${pagina};max-age=60*10;`;
 
         $("#datos")[0].innerHTML = "";
 
@@ -63,9 +62,8 @@ $(document).ready(() => {
                 <div class="col-sm-12 col-md-6 col-xl-3 border">
                     <div class="d-flex flex-column align-items-center">
                         <img src="${datos[i].foto}" alt="foto de perfil no encontrada" class="fotoPerfil rounded-circle img-fluid mt-1">
-                        <label for="Nomina">${datos[i].nomina}_${i}</label>
                         <label for="Nombre">${datos[i].nombre}_${i}</label>
-                        <label for="Carrera">${datos[i].carrera}_${i}</label>
+                        <label for="Integrantes">${datos[i].integrantes}_${i}</label>
                         <input name=${i} type="button" value="Editar" class="btn btn-success mb-2">
                     </div>
                 </div>`;
@@ -79,13 +77,13 @@ $(document).ready(() => {
     cargar();
 
     $("#cantidad").click(() => {
-        document.cookie = `cantidadDocente=${$("#mostrar")[0].value};max-age=60*10;`;
+        document.cookie = `cantidadAcademia=${$("#mostrar")[0].value};max-age=60*10;`;
         location.reload();
     });
 
 });
 
 const paginaRapida = (pagina) => {
-    document.cookie = `paginaDocente=${pagina - 1};max-age=60*10;`;
+    document.cookie = `paginaAcademia=${pagina - 1};max-age=60*10;`;
     location.reload();
 }
